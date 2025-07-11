be577d56a81af056b5cba4dbe10ec094a74a255c
Mike Bruzina <michael.bruzina@gmail.com>
2025-07-11 14:35:32 -0500

    added quicksort comments

--- sorting/quick.go
+++ sorting/quick.go
@@ -4,14 +4,17 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// QuickSort sorts the given slice of ordered items in-place using the default comparator.
 func QuickSort[T constraints.Ordered](items []T) {
 	copy(items, quickSort(items, DefaultComparator[T]{}))
 }
 
+// QuickSortWithComparator sorts the given slice of items in-place using a custom comparator.
 func QuickSortWithComparator[T any](items []T, comparator Comparator[T]) {
 	copy(items, quickSort(items, comparator))
 }
 
+// quickSort recursively sorts the slice using the provided comparator and returns a new sorted slice.
 func quickSort[T any](items []T, comparator Comparator[T]) []T {
 	if len(items) <= 1 {
 		return items
